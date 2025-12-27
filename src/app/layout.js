a0c5d5e73890ae0154d6bc44f8a5effcfa60b9b8
thmim <mdtaharimmim1010@gmail.com>
2025-12-27 11:47:28 +0600

    added site speed analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import BackGround from "./components/particleBg/BackGround";
 import InitialLoader from "./components/InitialLoader";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -37,6 +38,7 @@ export default function RootLayout({ children }) {
            {children} 
         </main>
         <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
